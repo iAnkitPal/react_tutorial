2daad7e0b9828995592851e3fbf50fe79a3d2368
Ankit Pal <ankit.pal@i2k2.com>
2024-07-22 20:34:25 +0530

    counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Counter from "./Counter";
 
 function App() {
   return (
     <>
       <Header></Header>
       <h2>React with vite</h2>
+      <Counter></Counter>
     </>
   );
 }
