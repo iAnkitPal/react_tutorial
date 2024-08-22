b1187ca7858356cd23006321668fc2af69e0a1ac
Ankit pal <a.300pal@gmail.com>
2024-08-22 23:52:50 +0530

    added ui elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
-// import { PasswordGenerator } from "./components/PasswordGenerator";
-import {CurrencyConvertor} from "./components/CurrencyConvertor";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-
-  // const demoObj = {
-  //   desc:"This is product One!"
-  // }
   return (
     <>
-      {/* <Card deviceName="Macbook" demoObj={demoObj} />
-      <Card deviceName="Lenovo"  demoObj={demoObj}/>
-      <Card deviceName="Dell"  demoObj={demoObj}/> */}
-      {/* <BgChanger /> */}
-      {/* <PasswordGenerator /> */}
-      <CurrencyConvertor />
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4"><TodoForm/></div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve:{
-  //   alias: [{ 
-  //     find: "@server", 
-  //     replacement: resolve(__dirname, './src/server/') 
-  //   }]
-  // }
 })
